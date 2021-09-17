4f0e4a3516417e12ed43d6a6a5423ef36eba9995
Tracy Ammann <tea1986@gmail.com>
2021-09-16 20:52:38 -0700

    added files

---
+++ app.py
@@ -0,0 +1,100 @@
+# import necessary libraries
+
+from config2 import username, password
+
+from sqlalchemy import create_engine, func, inspect
+
+
+
+
+database_path = f"{username}:{password}@ec2-52-45-179-101.compute-1.amazonaws.com:5432/d52f4bpmrc2lpv"
+engine = create_engine(f'postgresql://{database_path}')
+connection = engine.connect()
+
+############################ From Han-se's file
+# import necessary libraries
+from models import create_classes
+import os
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+import pandas as pd
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Database Setup
+#################################################
+
+from flask_sqlalchemy import SQLAlchemy
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '').replace("://", "ql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://waljilkidydguz:689b06ad421784ad4ea17abfa4d649ffe291a27c691edc52169cf33b166ffc3e@ec2-52-45-179-101.compute-1.amazonaws.com:5432/d52f4bpmrc2lpv'
+
+
+# Remove tracking modifications
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+castaways = create_classes(db)
+########################
+
+
+
+# # create route that renders index.html template
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+@app.route("/api/castaways.json")
+# def survivor():
+    # castaways_df = pd.read_sql('SELECT * FROM "Castaways"', connection)
+    # jsoncast = castaways_df.to_json(orient='records')
+    # print(jsoncast)
+
+    # return jsoncast
+
+    
+def survivor():
+    
+    # Query all columns in class
+
+    results = db.session.query(castaways).all()
+    all_castaways = []
+    # Loop through all records and add to dict
+    for result in results:
+        castaway_dict = {}
+        castaway_dict["id"] = result.id
+        castaway_dict["season_name"] = result.season_name
+        castaway_dict["season"] = result.season
+        castaway_dict["full_name"] = result.full_name
+        castaway_dict["castaway"] = result.castaway
+        castaway_dict["age"] = result.age
+        castaway_dict["age_group"] = result.age_group
+        castaway_dict["city"] = result.city
+        castaway_dict["state"] = result.state
+        castaway_dict["Lat"] = result.Lat
+        castaway_dict["Lon"] = result.Lon
+        castaway_dict["personality_type"] = result.personality_type
+        castaway_dict["personality_name"] = result.personality_name
+        # castaway_dict["day"] = result.day
+        # castaway_dict["order"] = result.order
+        castaway_dict["result"] = result.result
+        castaway_dict["total_votes_received"] = result.total_votes_received
+        castaway_dict["immunity_idols_won"] = result.immunity_idols_won
+        all_castaways.append(castaway_dict)
+    json_castaways = {"castaways": all_castaways}
+
+    return jsonify(json_castaways)
+
+if __name__ == "__main__":
+    app.run()
---
+++ templates/wordcloud.js
@@ -0,0 +1,133 @@
+
+// from: https://www.d3-graph-gallery.com/graph/wordcloud_size.html
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+// var allWords = [];
+// var myWordsSize = [];
+// d3.json("https://survivor-project2.herokuapp.com/api/castaways.json", function(data) {
+//     console.log(data);
+//     var castaways = data.castaways;
+//     // console.log(castaways);
+//     // var personality = castaways.personality_name;
+//     // console.log(personality);
+//     for (var i = 0; i < castaways.length; i++) {
+//      var personality = castaways[i].personality_name;
+//     //  console.log(personality);
+//      allWords.push(personality)
+//     }
+  
+// console.log(allWords);
+
+=======
+>>>>>>> parent of e239e23 (changes to index and app)
+=======
+>>>>>>> parent of e239e23 (changes to index and app)
+=======
+>>>>>>> parent of e239e23 (changes to index and app)
+// List of words
+var allWords = [];
+var myWordsSize = [];
+d3.json("castaways.json", function(data) {
+    // console.log(data.castaways);
+    var castaways = data.castaways;
+    // console.log(castaways);
+    // var personality = castaways.personality_name;
+    // console.log(personality);
+    for (var i = 0; i < castaways.length; i++) {
+     var personality = castaways[i].personality_name;
+    //  console.log(personality);
+     allWords.push(personality)
+    }
+  
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+// console.log(allWords);
+=======
+console.log(allWords);
+>>>>>>> parent of e239e23 (changes to index and app)
+=======
+console.log(allWords);
+>>>>>>> parent of e239e23 (changes to index and app)
+=======
+console.log(allWords);
+>>>>>>> parent of e239e23 (changes to index and app)
+
+// followed example in: https://stackoverflow.com/questions/48435191/how-do-i-build-an-object-counting-occurrences-in-an-array-in-javascript
+var myWords = {};
+for (var i = 0; i < allWords.length; i++) {
+  if (myWords.hasOwnProperty(allWords[i])) {
+    myWords[allWords[i]] += 1;
+  } else {
+    myWords[allWords[i]] = 1;
+  }
+}
+console.log(myWords);
+var myWordsarr = Object.keys(myWords).map(function (key){
+  return[key, myWords[key]];
+});
+console.log(myWordsarr);
+
+
+
+var myWordsNew = [];
+var fill = d3.scale.category20();
+var myWordColors = ['#FFE105', '#FF9900', '#7CFC00', '#48D1CC', '#A7FC00', '#FF7518', '#FFD700', '#E41B17', '#00A693', '#DFFF00', '#99FFFF', '#F400A1', '#FF4F00', '#7515D4', '#FFAA00', '#FFFF00', '#3366FF'];
+for (var j = 1; j < myWordsarr.length; j++) {
+  var addDict = {};
+  addDict["word"] = myWordsarr[j][0];
+  addDict["size"] = myWordsarr[j][1];
+  addDict["color"] = myWordColors[j];
+  console.log(addDict);
+  myWordsNew.push(addDict);
+}
+console.log(myWordsNew);
+
+
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 10, bottom: 10, left: 10},
+    width = 450 - margin.left - margin.right,
+    height = 450 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#word-cloud").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
+// Wordcloud features that are different from one word to the other must be here
+var layout = d3.layout.cloud()
+  .size([width, height])
+  .words(myWordsNew.map(function(d) { return {text: d.word, size:d.size}; }))
+  .padding(5)        //space between words
+  .rotate(function() { return ~~(Math.random() * 2) * 90; })
+  .fontSize(function(d) { return d.size; })      // font size of words
+  // .style("font-size", function(d) { return Math.min(2 * r, (2 * r - 8) / this.getComputedTextLength() * 24) + "px"; })
+  .on("end", draw);
+layout.start();
+
+
+// This function takes the output of 'layout' above and draw the words
+// Wordcloud features that are THE SAME from one word to the other can be here
+function draw(words) {
+  svg
+    .append("g")
+      .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+      .selectAll("text")
+        .data(words)
+      .enter().append("text")
+        .style("font-size", function(d) { return d.size; })
+        .style("fill", function(d, i) { return fill(i); })
+        .attr("text-anchor", "middle")
+        .style("font-family", "Impact")
+        .attr("transform", function(d) {
+          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; });
+    }
+  });
